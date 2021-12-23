b1c237739ab9be3e6d26df58e77f95d1482e6280
JensGamerYT <67007405+JensGamerYT@users.noreply.github.com>
2021-12-23 21:27:55 +0000

    init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 const botConfig = require("./botconfig.json");
  
 const client = new discord.Client();
-client.login(process.env.token)
+client.login(process.env.token);
  
 client.on("ready", async () => {
  
@@ -24,8 +24,8 @@ client.on("message", async message => {
  
     var command = messageArray[0];
  
-    if (command === `${prefix}twitch`) {
-        return message.channel.send("Mijn Twitch? https://www.twitch.tv/JensGamerLive !");
+    if (command === `${prefix}youtube`) {
+        return message.channel.send("Dit is Jens zijn Kanaal: https://www.youtube.com/c/JensGamerYT !");
     }
 
     if (command === `${prefix}mcinfo`) {
@@ -34,100 +34,24 @@ client.on("message", async message => {
             .setColor("#ff0339")
             .addFields(
                 {name: "IP:", value:"`play.jgserver.be`"},
-                {name: "Available Versions:", value:"1.16.5"},
+                {name: "Available Versions:", value:"1.15.2 --> 1.16.3"},
                 {name: "Status:", value:"OFFLINE"},
             )
             .setFooter("JG | Network Bot", "https://dunb17ur4ymx4.cloudfront.net/webstore/logos/a8415ba6cd7c3691a5ea49bdcc2ccf783ab93b55.png")    
 
         return message.channel.send(botEmbet);
-            }
-
+    }
     if (command === `${prefix}help`) {
         
         var botEmbet = new discord.MessageEmbed()
             .setColor("003cff")
             .addFields(
-                {name: "Twitch", value:"`*Twitch`"},
+                {name: "YouTube", value:"`*YouTube`"},
                 {name: "Minecraft Info:", value:"*mcinfo"},
             )
             .setFooter("JG | Network Bot", "https://dunb17ur4ymx4.cloudfront.net/webstore/logos/a8415ba6cd7c3691a5ea49bdcc2ccf783ab93b55.png")    
 
         return message.channel.send(botEmbet);
-        }
-
-        if (command === `${prefix}kick`) {
- 
-            const args = message.content.slice(prefix.length).split(/ +/);
-     
-            if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry jij kan dit niet");
-     
-            if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("Geen perms");
-     
-            if (!args[1]) return message.reply("Geen gebruiker opgegeven.");
-     
-            if (!args[2]) return message.reply("Gelieve een redenen op te geven.");
-     
-            var kickUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
-     
-            var reason = args.slice(2).join(" ");
-     
-            if (!kickUser) return message.reply("Kan de gebruiker niet vinden.");
-     
-            var embed = new discord.MessageEmbed()
-                .setColor("#ff0000")
-                .setThumbnail(kickUser.user.displayAvatarURL)
-                .setFooter(message.member.displayName, message.author.displayAvatarURL)
-                .setTimestamp()
-                .setDescription(`** Gekickt:** ${kickUser} (${kickUser.id})
-                **Gekickt door:** ${message.author}
-                **Redenen: ** ${reason}`);
-     
-            var embedPrompt = new discord.MessageEmbed()
-                .setColor("GREEN")
-                .setAuthor("Gelieve te reageren binnen 30 sec.")
-                .setDescription(`Wil je ${kickUser} kicken?`);
-     
-     
-            message.channel.send(embedPrompt).then(async msg => {
-     
-                var emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
-
-                if(emoji === "✅") { 
-
-                    msg.delete();
-
-                    kickUser.kick(reason).catch(err => {
-                        if (err) return message.reply("Er is iets foutelopen");
-                    });
-
-                    message.channel.send(embed);
-
-                }else if(emoji === "❌"){
-
-                    msg.delete();
-
-                    (await message.reply("Kick geanuleerd")).attachments(m => m.delete(5000));
-
-                }
-    
-            })
-
- 
-    }
+    }    
 
 });
-
-
-async function promptMessage(message, author, time, reactions){
-
-    time *= 1000;
-
-    for(const reaction of reactions){
-        await message.react(reaction);
-    } 
-
-    var filter = (reaction, user) => reaction.includes(reaction.emoji.name && user.id === author .id)
-
-    return message.awaitReactions(filter, {max:1, time: time}).then(collected => collected.first() && collected.first().emoji.name);
-
-}
